2614c14358e76af25341b1090bb756f90343fade
Clement Gil <clement_gil@orange.fr>
2017-05-29 11:20:45 +0200

    Add_Of_Forms

--- src/TwoLulzBundle/Controller/PostController.php
+++ src/TwoLulzBundle/Controller/PostController.php
@@ -3,10 +3,14 @@
 namespace TwoLulzBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \TwoLulzBundle\Entity\Post;
+use \TwoLulzBundle\Form\PostType;
 
 class PostController extends Controller{
     public function indexAction()
     {
-        return $this->render('TwoLulzBundle:Default:index.html.twig');
+        $post = new Post();
+        $form = $this->createForm(PostType::class,$post);
+        return $this->render('TwoLulzBundle:Default:index.html.twig', ['form' => $form->createView(),]);
     }
 }
---
+++ src/TwoLulzBundle/Form/PostType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace TwoLulzBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type as Form;
+//use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class PostType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title',Form\TextType::class)
+                ->add('image',Form\FileType::class);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'TwoLulzBundle\Entity\Post'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'twolulzbundle_post';
+    }
+
+
+}
