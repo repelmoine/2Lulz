1b584e2c848fb2df36938619a648250a46362727
Clement Gil <clement_gil@orange.fr>
2017-06-03 09:50:53 +0200

    Added_fonctionnalities_of_index

--- src/TwoLulzBundle/Controller/PostController.php
+++ src/TwoLulzBundle/Controller/PostController.php
@@ -14,6 +14,39 @@ class PostController extends Controller{
         $em = $this->getDoctrine()->getManager();
         $repository = $em->getRepository('TwoLulzBundle:Post');
         $allPosts = $repository->findAll();
-        return $this->render('TwoLulzBundle:Default:index.html.twig', ['form' => $form->createView(), 'posts' => $allPosts]);
+        
+        $repository2 = $em->getRepository('TwoLulzBundle:Comment');
+        $allComments = $repository2->findAll();
+        return $this->render('TwoLulzBundle:Default:index.html.twig', ['form' => $form->createView(), 'posts' => $allPosts, 'comments' => $allComments]);
+    }
+    
+    public function newAction(Request $request)
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $file stores the uploaded PDF file
+            /** @var Symfony\Component\HttpFoundation\File\UploadedFile $file */
+            $file = $post->getImage();
+
+            // Generate a unique name for the file before saving it
+            $fileName = md5(uniqid()).'.'.$file->guessExtension();
+
+            // Move the file to the directory where brochures are stored
+            $file->move(
+                $this->getParameter('images'),
+                $fileName
+            );
+
+            // Update the 'brochure' property to store the PDF file name
+            // instead of its contents
+            $post->setImage($fileName);
+
+            // ... persist the $product variable or any other work
+
+            return $this->forward('TwoLulzBundle:Post:index');
+        }
     }
 }
